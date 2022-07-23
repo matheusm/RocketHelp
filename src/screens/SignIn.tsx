31f138119a264a7b1d011a837d7a48d117c968c9
Matheus Martins <matheusms280100@gmail.com>
2022-07-23 00:49:18 -0300

    feat(signin) add phosphor icons and signin page

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,8 +1,40 @@
-import { View, Text } from "react-native"
+import { useState } from "react";
+import { Heading, Icon, VStack, useTheme } from "native-base";
+import { Envelope, Key } from "phosphor-react-native";
+
+import Logo from '../assets/logo_primary.svg';
+import { Button } from "../components/Button";
+import { Input } from "../components/Input";
+
 export function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const { colors } = useTheme();
+
   return (
-    <View>
-      <Text>Olá, meu bom</Text>
-    </View>
+    <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
+      <Logo />
+      <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
+        Acesse sua conta
+      </Heading>
+
+      <Input
+        placeholder="E-mail"
+        mb={4}
+        InputLeftElement={<Icon as={<Envelope color={colors.gray[300]} />} ml={4} />}
+        onChangeText={setEmail}
+      />
+
+      <Input
+        mb={8}
+        placeholder="Senha"
+        InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
+        secureTextEntry
+        onChangeText={setPassword}
+      />
+
+      <Button title="Entrar" w="full" />
+    </VStack>
   )
 }
